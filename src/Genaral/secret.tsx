f09f3ef7f581e97c633005673a7faa620bcb35a9
eswaraprasad <esweraprasad@gmail.com>
2026-02-28 11:37:28 +0530

    update code on

--- src/Genaral/secret.tsx
+++ src/Genaral/secret.tsx
@@ -3,8 +3,8 @@ export const whatsapp_number = "+916363600791";
 export const phone_number2 = "+917676384744";
 export const whatsapp_number2 = "+917676384744";
 
-export const business_name = "Kerala Bhagavathi Astro";
-export const person_name = "Pandith Raghunandan Poduval";
+export const business_name = "Kerala Astro";
+export const person_name = "Pandith Sai Sasthan Guruji";
 
 export const email_address = "";
 
